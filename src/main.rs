e2b0edcbde9713b7bdbd3adb4ff45eff6dbedd6a
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-09-10 19:57:28 +0200

    feat(color): basic colored output

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ enum Error {
     ExecFailure(String, usize),
 }
 
+const RED: &str = "\x1b[0;31m";
+const GREEN: &str = "\x1b[0;32m";
+const BOLD: &str = "\x1b[1m";
+const FAINT: &str = "\x1b[2;37m";
+const RESET: &str = "\x1b[m";
+
 impl std::fmt::Debug for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -80,10 +86,13 @@ fn main() -> Result<(), Error> {
                     .map_err(|err| Error::ExecRunError(err, program_and_args(&mut process), line_number))?;
 
                 if !output.status.success() {
-                    return Err(Error::ExecFailure(program_and_args(&mut process), line_number));
+                    let program_and_args = program_and_args(&mut process);
+                    return failure(&mut out, &file_name, line_number, &program_and_args);
+                } else {
+                    success(&mut out, &file_name, line_number)?;
                 }
 
-                write!(out, "✅{file_name}: code block l{line_number} - PASS\n").map_err(Error::IoWriteError)?;
+                cmd.clear();
             }
 
             line.clear();
@@ -97,11 +106,45 @@ fn main() -> Result<(), Error> {
 fn program_and_args(process: &mut std::process::Command) -> String {
     let program = process.get_program();
     let mut program_and_args = process.get_args().fold(String::new(), |mut acc, arg| {
-        // acc.push(' ');
-        // acc.push_str(&arg.to_string_lossy());
+        acc.push(' ');
+        acc.push_str(&arg.to_string_lossy());
         acc
     });
 
     program_and_args.insert_str(0, &program.to_string_lossy());
     program_and_args
+}
+
+fn success(out: &mut impl std::io::Write, file_name: &str, line_number: usize) -> Result<(), Error> {
+    write!(
+        out,
+        "\
+            ✅{BOLD}{file_name}{RESET}: \
+            code block at line {line_number} - \
+            {GREEN}PASS{RESET}\n\
+        "
+    )
+    .map_err(Error::IoWriteError)
+}
+
+fn failure(
+    out: &mut impl std::io::Write,
+    file_name: &str,
+    line_number: usize,
+    program_and_args: &str,
+) -> Result<(), Error> {
+    write!(
+        out,
+        "\
+            ❌{BOLD}{file_name}{RESET}: \
+            code block at line {line_number} - \
+            {RED}FAIL{RESET}\n\
+            {FAINT}\
+            ```\n\
+            {program_and_args}\n\
+            ```\n\
+            {RESET}\
+        "
+    )
+    .map_err(Error::IoWriteError)
 }
